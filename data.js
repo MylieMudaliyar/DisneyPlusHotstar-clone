a6692c320dd0547bfa052940180d581183a19870
Mylie <myliemudaliyar@gmail.com>
2022-08-26 10:19:07 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 2.PNG",
+  },
+  {
+    name: "loki",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 1.PNG",
+  },
+  {
+    name: "wanda vision",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 3.PNG",
+  },
+  {
+    name: "raya and the last dragon",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 4.PNG",
+  },
+  {
+    name: "luca",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 5.PNG",
+  },
+];
